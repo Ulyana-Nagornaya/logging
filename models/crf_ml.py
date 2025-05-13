7d29c5820e0807caee52389fde370f8824818608
Ulyana-Nagornaya <ulyana.nagornaya@gmail.com>
2025-05-13 10:35:33 +0300

    ml

--- models/crf_ml.py
+++ models/crf_ml.py
@@ -1,7 +1,70 @@
 from sklearn_crfsuite import CRF
 from sklearn.metrics import classification_report
+from utils import generate_bio_tags
+import spacy
 
+nlp = spacy.load("en_core_web_sm")
 
+def add_pos_tags(row):
+    tokens = row["tokens"]
+    doc = nlp(" ".join(tokens))
+    row["pos_tags"] = [token.pos_ for token in doc]
+    return row
+
+def get_char_ngrams(token, min_n=2, max_n=5):
+    ngrams = set()
+    for n in range(min_n, max_n + 1):
+        for i in range(len(token) - n + 1):
+            ngrams.add(token[i:i + n])
+    return ngrams
+
+def extract_features(df_row, i):
+    tokens = df_row["tokens"]
+    pos_tags = df_row["pos_tags"]
+    token = tokens[i]
+    pos = pos_tags[i]
+    features = {
+        "bias": 1.0,
+        "pos": pos,
+        "word.lower()": token.lower(),
+        "is_capitalized": token[0].isupper() if len(token) > 0 else False,
+        "is_all_caps": token.isupper(),
+        "word.is_punctuation": token in [".", ",", ";", "?", "!"],
+        "prefix4": token[:4] if len(token) >= 4 else token,
+        "suffix4": token[-4:] if len(token) >= 4 else token,
+    }
+    if i > 0:
+        prev_token = tokens[i - 1]
+        prev_pos = pos_tags[i - 1]
+        features.update({
+            "-1:word.lower()": prev_token.lower(),
+            "-1:pos": prev_pos if len(prev_pos) >= 2 else prev_pos,
+        })
+    else:
+        features["BOS"] = True
+    if i < len(tokens) - 1:
+        next_token = tokens[i + 1]
+        next_pos = pos_tags[i + 1]
+        features.update({
+            "+1:word.lower()": next_token.lower(),
+            "+1:pos": next_pos if len(next_pos) >= 2 else next_pos,
+        })
+    else:
+        features["EOS"] = True
+    return features
+
+def sent2features(df_row):
+    return [extract_features(df_row, i) for i in range(len(df_row["tokens"]))]
+
+def sent2labels(df_row):
+    return df_row["bio_tags"]
+
+def prepare_data(df):
+    df = df.apply(add_pos_tags, axis=1)
+    df["bio_tags"] = df.apply(lambda row: generate_bio_tags(row["tokens"], row["entities"]), axis=1)
+    X = df.apply(sent2features, axis=1).tolist()
+    y = df.apply(sent2labels, axis=1).tolist()
+    return  df, X, y
 class CRFModel:
     def __init__(self):
         self.model = CRF(
